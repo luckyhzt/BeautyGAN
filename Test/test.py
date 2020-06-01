1c4e34b8754163f67952ad2da9236b64191d660d
Tonis <luckyhzt@gmail.com>
2020-06-01 18:41:10 -0400

    add Test

--- Test/test.py
+++ Test/test.py
@@ -44,9 +44,9 @@ def main():
     testset = FBP_dataset_V2('test', config['SCUT-FBP-V2'], config['test_index'], config['img_size'], config['crop_size'])
     sampler = Label_Sampler(config['SCUT-FBP-V2'], config['train_index'])
     # Show result
-    #show_index = [120, 121, 122]
-    #show_score = np.array([1.5, 2.5, 3.5, 4.5])
-    #display_result(generator, testset, show_index, show_score)
+    show_index = range(200)
+    show_score = np.array([1.25, 1.5, 1.75, 2.0, 2.25, 2.5, 2.75, 3.0, 3.25, 3.5, 3.75, 4.0, 4.25, 4.5, 4.75])
+    save_result(generator, testset, show_index, show_score)
     # Test idenity loss
     #beauty_loss, identity_loss = test_loss(generator, FBP, FRN, trainset, sampler, 20)
     #print(beauty_loss, identity_loss)
@@ -79,6 +79,7 @@ def display_result(generator, dataset, index, score):
     result = []
     for i in index:
         x, y = dataset[i]
+        print(str(i) + ' : ', str(y[0]))
         x = x.unsqueeze(0)
         x = x.cuda()
         x_g = x.repeat(score.shape[0], 1, 1, 1)
@@ -91,10 +92,41 @@ def display_result(generator, dataset, index, score):
     utils.display_img(result, 'generated faces')
 
 
+def save_result(generator, dataset, index, score):
+    for i in index:
+        x, y = dataset[i]
+        name = '%.4f' % np.round(y[0], 4)
+        save_path = os.path.join(config['image_save_path'], name)
+        if not os.path.isdir(save_path):
+            os.mkdir(save_path)
+        print(str(i) + ' : ', str(y[0]))
+        x = x.unsqueeze(0)
+        x = x.cuda()
+        x_g = x.repeat(score.shape[0], 1, 1, 1)
+        y_g = torch.cuda.FloatTensor(score.reshape([-1, 1]))
+        x_g = generator(x_g, y_g)
+        # Save original image
+        save_tensor_as_image(x[0], os.path.join(save_path, 'original.png'))
+        # Save generated image
+        for k in range(score.shape[0]):
+            save_tensor_as_image(x_g[k], os.path.join(save_path, str(score[k]) + '.png'))
+
+
+
+def save_tensor_as_image(tensor, path):
+    if tensor.is_cuda:
+        tensor = tensor.cpu()
+    tensor = (tensor + 1) / 2
+    img = torchvision.transforms.ToPILImage()(tensor)
+    img.save(path)
+
+
+
+
 def load_config():
     thisDir = os.path.dirname(__file__)
     result_path = 'D:/Program/BeautyGAN/Train/Exp_3_cycle/Result'
-    experiment = 'Exp_11'
+    experiment = 'Final'
     param = 'generator_300.pth'
 
     exp_path = os.path.join(result_path, experiment)
@@ -103,6 +135,7 @@ def load_config():
     
     config['exp_path'] = exp_path
     config['param_path'] = os.path.join(exp_path, param)
+    config['image_save_path'] = 'Test/Result'
 
     return config
 
